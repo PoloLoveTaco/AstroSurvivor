8403f3e22497976abcc29f6a5ffe156049ff2ca0
Polo <jules.trolle@epitech.eu>
2025-10-30 14:15:31 +0100

    [ADD] shooting each seconds

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -4,3 +4,8 @@ extends CharacterBody2D
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	if _body.is_in_group("Enemy"):
+		self.queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,23 +6,24 @@ const BULLET = preload("uid://7vwsap1y2sw5")
 @export var health: float = 100
 @export var speed: float = 400
 @export var xp: float = 0
+@export var seconds_between_attacks: float = 1
 
 @export_category("Max stats")
 @export var max_health: float = 100
 @export var max_xp: float = 10
 
 @onready var progress_bar: ProgressBar = $CanvasLayer/UI/ProgressBar
+@onready var shooting_timer: Timer = $"Shooting Timer"
 
 func _ready() -> void:
 	progress_bar.value = health / max_health * 100
+	shooting_timer.wait_time = seconds_between_attacks
+	shooting_timer.start()
 
 func get_input():
 	look_at(get_global_mouse_position())
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-	
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
 
 func _physics_process(_delta):
 	get_input()
@@ -33,3 +34,8 @@ func shoot():
 	b.global_position = global_position
 	b.velocity = (get_global_mouse_position() - global_position).normalized() * b.speed
 	get_tree().current_scene.add_child(b)
+
+
+func _on_shooting_timer_timeout() -> void:
+	shoot()
+	shooting_timer.start()
