c6a89f3bb624f44629e074905df60daf98ef0c03
Polo <jules.trolle@epitech.eu>
2025-10-30 15:23:16 +0100

    [ADD] add background

--- Scripts/test_map.gd
+++ Scripts/test_map.gd
@@ -2,6 +2,16 @@ extends Node2D
 
 const CURSOR = preload("res://Assets/cursor.png")
 
+@onready var stars := $"StarBackground" # ton ColorRect
+var mat: ShaderMaterial
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(CURSOR)
+	mat = stars.material as ShaderMaterial
+
+func _process(_dt):
+	var cam := get_viewport().get_camera_2d()
+	if cam and mat:
+		# envoie la position monde de la caméra au shader
+		mat.set_shader_parameter("camera_offset", cam.global_position)
